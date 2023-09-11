528389dac54ed796049d3c05d95198d50aa4309b
Young Jang <jyoungjoon@gmail.com>
2023-09-10 22:52:16 -0700

    ADD: shadow

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -104,11 +104,13 @@ const AboutPicture = styled.div`
   background-position: center;
   border-radius: 0rem 0rem 0rem 5rem;
   transform: translate(0%, -5%);
-  filter: opacity(0.8);
+  filter: opacity(0.8) brightness(0.975);
   position: fixed;
   top: 0;
   right: 0;
   z-index: -1;
+  box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
+    rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
 
   @media ${device.laptopL} {
     background-position: right;
